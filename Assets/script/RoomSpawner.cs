985102b51a53a7075f4647518250e405193f2d01
SUSSEXDOWNS\467703 <467703@sussexdowns.ac.uk>
2018-11-28 14:38:02 +0000

    random room near done

--- Assets/script/RoomSpawner.cs
+++ Assets/script/RoomSpawner.cs
@@ -4,13 +4,54 @@ using UnityEngine;
 
 public class RoomSpawner : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    public int openingDirection;
+    //1 = bottom door
+    //2 = top door
+    //3 = left door
+    //4 = right door
+
+    private RoomTemplates templates;
+    private int rand;
+    private bool spawned = false;
+
+    void Start()
+    {
+        templates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomTemplates>();
+        Invoke("Spawn", 0.1f);
+    }
+	void Spawn(){
+        if (spawned == false) {
+            if (openingDirection == 1)
+            {
+                //need bottom door
+                rand = Random.Range(0, templates.bottomRooms.Length);
+                Instantiate(templates.bottomRooms[rand], transform.position, templates.bottomRooms[rand].rotation);
+            }
+            else if (openingDirection == 2)
+            {
+                //need top door
+                rand = Random.Range(0, templates.topRooms.Length);
+                Instantiate(templates.topRooms[rand], transform.position, templates.topRooms[rand].rotation);
+            }
+            else if (openingDirection == 3)
+            {
+                //need left door
+                rand = Random.Range(0, templates.leftRooms.Length);
+                Instantiate(templates.leftRooms[rand], transform.position, templates.leftRooms[rand].rotation);
+            }
+            else if (openingDirection == 4)
+            {
+                //need right door
+                rand = Random.Range(0, templates.rightRooms.Length);
+                Instantiate(templates.rightRooms[rand], transform.position, templates.rightRooms[rand].rotation);
+            }
+            spawned = true;
+        }
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void OnTriggerEnter2D(Collider2D other) {
+        if(other.CompareTag("SpawnPoint") && other.GetComponent<RoomSpawner>().spawned == true)
+        {
+            Destroy(gameObject);
+        }
+    }
 }
